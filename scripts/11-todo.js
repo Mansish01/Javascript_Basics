25dc67d6a265e86d4d2c0329edb2216c509e5fc8
MANISH <gyawalimanish12345@gmail.com>
2024-01-23 21:11:48 +0545

    updated changes

--- scripts/11-todo.js
+++ scripts/11-todo.js
@@ -7,10 +7,21 @@ function renderTodo(){
     let todolistHTML = '';
 
     for (let i = 0; i<todoList.length; i++){
-        const todo = todoList[i];
+        const todoObject = todoList[i];
 
-        const html = `<p> ${todo}</p>`;
+        const name = todoObject.name;
+        const date = todoObject.deuDate;
+
+        const html = `
+            <div>${name} </div>
+            <div>${date} </div>
+            <button onclick = "
+                todoList.splice(${i}, 1);
+                renderTodo();
+          " class="delete-btn">Delete</button>
+        `;
         //html generation  
+
         
         todolistHTML += html;
     }
@@ -21,11 +32,19 @@ function renderTodo(){
         .innerHTML = todolistHTML;
 }
 function addTodo (){
-    const inputElement = document.querySelector('.js-inputtask')
-    const task = inputElement.value
+    const inputElement = document.querySelector('.js-inputtask');
+    const task = inputElement.value;
     // console.log(task)
-    todoList.push(task)
-    console.log(todoList)
+
+    const inputDate = document.querySelector('.js-duedate');
+    const date = inputDate.value;
+
+    todoList.push({
+        name : task,
+        deuDate : date,
+    })
+
+    // console.log(todoList)
 
     inputElement.value = '';
 
